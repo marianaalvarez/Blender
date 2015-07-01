a4d25234e46c4e10645ccc00164a1f12ef7daf8d
Mariana Alvarez <marianaalvarezdecarvalho@hotmail.com>
2015-07-01 03:16:07 -0300

    Criação de telas.

--- Blender/Blender/AppDelegate.swift
+++ Blender/Blender/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        UITabBar.appearance().tintColor = UIColor(red:1, green:0.41, blue:0.617, alpha:1)
         return true
     }
 
